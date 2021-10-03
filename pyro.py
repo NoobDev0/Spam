88130a686ca82953fe35a7a2ec2752f8d27fe9ff
Noob Dev <88297858+NoobDev0@users.noreply.github.com>
2021-10-03 20:13:51 +0530

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -1,7 +1,8 @@
 from pyrogram import Client
+from . import API_HASH, APP_ID
 
-api_id = 5972952
-api_hash = "74536bd24d8ad4d2e51445d07c41b0bb"
+api_id = APP_ID
+api_hash = API_HASH 
 while True:
     with Client("vinesh_1981", api_id, api_hash) as app:
         app.send_message("vinesh1981", "Greetings from **Pyrogram**!")
